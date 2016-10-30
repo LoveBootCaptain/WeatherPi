beff64102ea142d263b08111da0df5d50e2c5126
lovebootcaptain <loveboatcaptain77@googlemail.com>
2016-10-30 02:20:23 +0200

    - simplified drawing to unicorn, also saves some lines of code

--- Modules/update_unicorn.py
+++ Modules/update_unicorn.py
@@ -15,9 +15,9 @@ config = get_config()
 
 THREADING_TIMER = config['THREADING_TIMER']
 
-img = '/home/pi/WeatherPi/Modules/Animations/error.png'
+img_file = '/home/pi/WeatherPi/Modules/Animations/error.png'
 
-base_path = '/home/pi/WeatherPi/Modules/Animations/'
+folder_path = '/home/pi/WeatherPi/Modules/Animations/'
 
 icon_extension = '.' + 'png'
 
@@ -39,25 +39,25 @@ def get_icon_path():
 
     # threading.Timer(10, get_icon_path).start()
 
-    global img
+    global img_file
 
     icon = get_icon()
 
-    icon_path = base_path + icon + icon_extension
+    icon_path = folder_path + icon + icon_extension
 
-    log_string('The Icon Path should be: {}. Checking it...'.format(icon_path))
+    log_string('The icon path should be: {}. Checking it...'.format(icon_path))
 
     # check if file and path are valid
 
     if os.path.isfile(icon_path):
 
-        log_string('The File: {} is valid and present'.format(icon_path))
+        log_string('The File: {} is valid and present!'.format(icon_path))
 
-        log_string('Updating Unicorn with new weather condition Icon')
+        log_string('Updating Unicorn with new weather condition icon: {}.'.format(icon))
 
         # set new icon_path
 
-        img = icon_path
+        img_file = icon_path
 
         blink('red')
 
@@ -65,71 +65,63 @@ def get_icon_path():
 
         log_string('Error - there is no icon for this weather condition. Please check.')
 
-        log_string('Updating Unicorn Icon to Error Screen')
+        log_string('Updating Unicorn icon to error screen')
 
         # set new icon_path
 
         icon_path = '/home/pi/WeatherPi/Modules/Animations/error.png'
 
-        img = icon_path
+        img_file = icon_path
 
         blink('red')
 
 
 def update_unicorn():
 
-    global img
+    global img_file
 
     unicorn.clear()
 
-    log_string('Start Unicorn Image Loop')
+    log_string('Start Unicorn image loop')
 
-    while img:
+    while img_file:
 
-        my_img = Image.open(img)
+        img = Image.open(img_file)
 
-        for o_x in range(int(my_img.size[0] / 8)):
+        draw_unicorn(img)
 
-            for o_y in range(int(my_img.size[1] / 8)):
 
-                for x in range(8):
+def draw_unicorn(image):
 
-                    for y in range(8):
-                        pixel = my_img.getpixel(((o_x * 8) + y, (o_y * 8) + x))
-                        # print(pixel)
-                        r, g, b = int(pixel[0]), int(pixel[1]), int(pixel[2])
-                        unicorn.set_pixel(x, y, r, g, b)
+    for o_x in range(int(image.size[0] / 8)):
 
-                unicorn.show()
-                time.sleep(0.25)
+        for o_y in range(int(image.size[1] / 8)):
+
+            for x in range(8):
+
+                for y in range(8):
+                    pixel = image.getpixel(((o_x * 8) + y, (o_y * 8) + x))
+                    # print(pixel)
+                    r, g, b = int(pixel[0]), int(pixel[1]), int(pixel[2])
+                    unicorn.set_pixel(x, y, r, g, b)
+
+            unicorn.show()
+            time.sleep(0.25)
 
 
 def test_unicorn():
 
-    print('Testing all images in folder {}'.format(base_path))
+    print('Testing all images in folder {}'.format(folder_path))
 
-    for file in os.listdir(base_path):
+    for file in os.listdir(folder_path):
 
         if file.endswith(icon_extension):
 
-            print('Testing image: {}'.format(base_path + file))
+            print('Testing image: {}'.format(folder_path + file))
 
-            file = Image.open(base_path + file)
+            img = Image.open(folder_path + file)
 
-            for o_x in range(int(file.size[0] / 8)):
-
-                for o_y in range(int(file.size[1] / 8)):
-
-                    for x in range(8):
-
-                        for y in range(8):
-                            pixel = file.getpixel(((o_x * 8) + y, (o_y * 8) + x))
-                            # print(pixel)
-                            r, g, b = int(pixel[0]), int(pixel[1]), int(pixel[2])
-                            unicorn.set_pixel(x, y, r, g, b)
-
-                    unicorn.show()
-                    time.sleep(0.25)
+            draw_unicorn(img)
 
         else:
 
