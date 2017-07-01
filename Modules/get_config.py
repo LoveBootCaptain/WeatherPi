5c339635687238ce91889cb4e7e433cd6413f0e6
lovebootcaptain <loveboatcaptain77@googlemail.com>
2017-07-01 21:38:44 +0200

    - logging for the config module

--- Modules/get_config.py
+++ Modules/get_config.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import json
 
+from init_logging import *
+
 # read the config file
 config_data = open('/home/pi/WeatherPi/config.json').read()
 
@@ -9,5 +11,7 @@ config_data = open('/home/pi/WeatherPi/config.json').read()
 def get_config():
 
     config = json.loads(config_data)
+
+    log_string('config file read by module')
 
     return config
