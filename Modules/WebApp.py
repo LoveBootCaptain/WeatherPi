4e1afff213fa1f5f76025b6dcf8f8c4c92d4dc15
locutus <loveboatcaptain77@googlemail.com>
2018-02-09 18:42:47 +0100

    new feature: little bootstrap websites for the API Data

--- Modules/WebApp.py
+++ Modules/WebApp.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, render_template
 from multiprocessing import Process
 from init_logging import log_string
 from Endpoint import Endpoint
+from init_blinkt import blink
 
 app = Flask(__name__)
 
@@ -23,42 +24,50 @@ class WebApp(Process):
 
 @app.route('/api/node')
 def get_unicorn_pi_data():
+    blink('yellow')
     return jsonify(Endpoint().unicorn_pi_data())
 
 
 @app.route('/api/sensors')
 def get_sensor_data():
+    blink('yellow')
     return jsonify(Endpoint().sensor_module_data())
 
 
 @app.route('/api/rpi')
 def get_rpi_stats():
+    blink('yellow')
     return jsonify(Endpoint().rpi_stats())
 
 
 @app.route('/')
 def index():
+    blink('yellow')
     return render_template('home.html')
 
 
 @app.route('/node')
 def node():
+    blink('yellow')
     return render_template('node.html', node_data=Endpoint().unicorn_pi_data())
 
 
 @app.route('/sensors')
 def sensors():
+    blink('yellow')
     return render_template('sensors.html', sensor_data=Endpoint().sensor_module_data())
 
 
 @app.route('/rpi')
 def rpi():
+    blink('yellow')
     return render_template('rpi.html', rpi_data=Endpoint().rpi_stats())
 
 
-@app.route('/base')
-def base():
-    return render_template('base.html')
+# @app.route('/base')
+# def base():
+#     blink('yellow')
+#     return render_template('base.html')
 
 
 log_string('api endpoints created')
